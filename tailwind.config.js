50789771df40fdde94ab8c6c99b14501720fbaa9
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-14 11:08:04 +0100

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5a60ae",
+        pinkBtn: " #ff6dcd",
+      },
+    },
   },
   plugins: [],
 };

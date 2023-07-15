a4d599c42d45d2d06442580d33f678d40fbfa01a
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-15 15:24:15 +0100

    Update footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-primary text-white py-8 px-4">
+      <p> &copy; 2023 Ojoachele Onuh. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;

4ca818ca0265e08d12e55e70c5fc700d37491d30
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-19 21:00:39 +0100

    Added custom screen size for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        xs: "279px",
+        // => @media (min-width: 279px) {...}
+      },
       colors: {
         primary: "#5a60ae",
         pinkBtn: " #ff6dcd",

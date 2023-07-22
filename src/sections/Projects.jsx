6be39de85721adfc52963f978777d25cc95642e3
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-22 07:16:53 +0100

    update style for medium screen

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
   return (
     <>
       <section className="bg-blue-400 bg-opacity-10 py-8 px-6" id="projects">
-        <h1 className="font-bold uppercase py-4 pl-14 lg:mx-16">projects</h1>
+        <h1 className="font-bold uppercase py-4 pl-14 lg:mx-40 ">projects</h1>
         {/* <section className="projects"> */}
         <div className="w-4/5 lg:w-1/2 lg:mb-6 max-w-full h-full flex flex-col sm:flex-row  items-center justify-center shadow-md rounded-lg overflow-auto p-4 my-2 mx-auto bg-white">
           <img
@@ -30,13 +30,13 @@ const Projects = () => {
               institutions decide who is best fit to get a loan without any
               interference and bias.
             </p>
-            <div className="sm:flex gap-2 sm:flex-col md:flex-row lg:flex-row">
+            <div className="xs:py-3 sm:flex gap-2 sm:flex-col md:flex-row lg:flex-row">
               <span>
                 <a
                   href="https://smartlend-app.netlify.app"
                   rel="noreferrer"
                   target="_blank"
-                  className="text-white border rounded-full bg-pinkBtn py-1 px-4  md:px-4 sm:text-xs sm:mb-3 lg:px-4 lg:text-base hover:bg-primary hover:text-white"
+                  className="text-white border rounded-full bg-pinkBtn py-1 px-4  md:px-4 sm:text-xs sm:mb-3 lg:px-4 lg:text-base hover:bg-primary hover:text-white xs:mr-2"
                 >
                   Live site
                 </a>
@@ -73,13 +73,13 @@ const Projects = () => {
               account, shorten long url, copy shortened link and get analytics
               of the created link.
             </p>
-            <div className="sm:flex gap-2 sm:flex-col md:flex-row lg:flex-row">
+            <div className="xs:py-3 sm:flex gap-2 sm:flex-col md:flex-row lg:flex-row">
               <span>
                 <a
                   href="https://shortly-21c95.firebaseapp.com/"
                   rel="noreferrer"
                   target="_blank"
-                  className="text-white border rounded-full bg-pinkBtn py-1 px-4 md:px-4 md:text-xs sm:text-xs sm:mb-3 lg:px-4 lg:text-base hover:bg-primary hover:text-white"
+                  className="text-white border rounded-full bg-pinkBtn py-1 px-4 md:px-4 md:text-xs sm:text-xs sm:mb-3 lg:px-4 lg:text-base hover:bg-primary hover:text-white xs:mr-2"
                 >
                   Live site
                 </a>
@@ -116,13 +116,13 @@ const Projects = () => {
               Achez mall is an online shopping store created with Vue, that
               consumes the dummyjson API and authenticates user with firebase
             </p>
-            <div className="sm:flex gap-2 sm:flex-col md:flex-row lg:flex-row">
+            <div className="xs:py-3 sm:flex gap-2 sm:flex-col md:flex-row lg:flex-row">
               <span>
                 <a
                   href="https://achez-mall.netlify.app/"
                   rel="noreferrer"
                   target="_blank"
-                  className="text-white border rounded-full bg-pinkBtn py-1 px-4 md:text-xs md:px-4 sm:text-xs sm:mb-3 lg:px-4 lg:text-base hover:bg-primary hover:text-white"
+                  className="text-white border rounded-full bg-pinkBtn py-1 px-4 md:text-xs md:px-4 sm:text-xs sm:mb-3 lg:px-4 lg:text-base hover:bg-primary hover:text-white xs:mr-2"
                 >
                   Live site
                 </a>
@@ -160,13 +160,13 @@ const Projects = () => {
               and details of a single repo. The Github api was consumed to
               create app.
             </p>
-            <div className="sm:flex gap-2 sm:flex-col md:flex-row lg:flex-row">
+            <div className="xs:py-3 sm:flex gap-2 sm:flex-col md:flex-row lg:flex-row">
               <span>
                 <a
                   href="https://altschool-exam-achele.vercel.app/"
                   rel="noreferrer"
                   target="_blank"
-                  className="text-white border rounded-full bg-pinkBtn py-1 px-4 md:px-4 md:text-xs  sm:text-xs sm:mb-3 lg:px-4 lg:text-base hover:bg-primary hover:text-white"
+                  className="text-white border rounded-full bg-pinkBtn py-1 px-4 md:px-4 md:text-xs  sm:text-xs sm:mb-3 lg:px-4 lg:text-base hover:bg-primary hover:text-white xs:mr-2"
                 >
                   Live site
                 </a>
